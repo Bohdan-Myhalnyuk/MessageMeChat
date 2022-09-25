02c95a98a80da8c6f4f6dbd48c275a06a6b4f0d5
Ubuntu <ubuntu@ip-172-31-83-74.ec2.internal>
2022-09-25 12:33:31 +0000

    Adding restrictions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
-  def new; end
+  before_action :logged_in_redirect, only: [:new, :create]
+  
+  
+  def new 
+  end
 
   def create
     @user = User.find_by(params[:session][:username])
@@ -18,3 +22,12 @@ class SessionsController < ApplicationController
     flash[:success] = 'You have successfuly logged out'
   end
 end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end 
+  end
