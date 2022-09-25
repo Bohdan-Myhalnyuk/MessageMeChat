ff6c9e2dd9409da4ecec8b6c6bfaca69be35838e
Ubuntu <ubuntu@ip-172-31-83-74.ec2.internal>
2022-09-25 12:02:14 +0000

    Working with models

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,20 @@ class SessionsController < ApplicationController
 	end
 	
 	def create
-		
+		@user = User.find_by(params[:session][:username])
+		if @user && @user.authenticate(params[:session][:password])
+			session[:user_id] = @user.id
+			flash[:success] = "You have successfuly logged in"
+			redirect_to root_path
+		else
+			flash.now[:error] = "There is something wrong with your login info"
+			render 'new'
+		end
+	end
+	
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "You have successfuly logged out"
 	end
 	
 end 
